1b0948f40f67e8d4b1ea1463123cbc86219407d9
Raydberg Gabriel <raydbergg@gmail.com>
2025-09-24 22:03:55 -0500

    first commit

---
+++ spring-container/src/main/java/com/app/HelloController.java
@@ -0,0 +1,16 @@
+package com.app;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/saludo")
+public class HelloController {
+    @GetMapping
+    public ResponseEntity<String> sakudo() {
+        return ResponseEntity.ok("Hola mundo desde contenedor de docker");
+    }
+
+}
